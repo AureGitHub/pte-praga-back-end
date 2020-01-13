0388099e87afb581ddb54e7d529ff4f23c9d04d5
AureGitHub <auredeveloper@gmail.com>
2020-01-13 11:37:15 +0100

    con sqllite

--- db/index.js
+++ db/index.js
@@ -1,21 +1,21 @@
 const path = require('path');
 const knex = require('knex');
 
-module.exports = knex({
-    client: 'mysql',
-    connection: { 
-      host: 'sql2.freemysqlhosting.net',
-      user: 'sql2318796',
-      password: 'bT7*rD4*',
-      database: 'sql2318796',
-      port: 3306,
+// module.exports = knex({
+//     client: 'mysql',
+//     connection: { 
+//       host: 'sql2.freemysqlhosting.net',
+//       user: 'sql2318796',
+//       password: 'bT7*rD4*',
+//       database: 'sql2318796',
+//       port: 3306,
     
-    },
+//     },
+//     useNullAsDefault: true,
+//   });
+
+  module.exports = knex({
+    client: 'sqlite3',
+    connection: { filename: path.join(__dirname, 'ppraga.db') },
     useNullAsDefault: true,
   });
-
-  // module.exports = knex({
-  //   client: 'sqlite3',
-  //   connection: { filename: path.join(__dirname, 'ppraga.db') },
-  //   useNullAsDefault: true,
-  // });
